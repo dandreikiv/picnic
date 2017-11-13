19f3e9951381fddf3c2aaecf479b54d46e7941c6
Dmytro Andreikiv <dmytro.andreikiv@philips.com>
2017-11-13 23:34:09 +0100

    Added ProductCellModel

---
+++ Picnic/Views/ProductCellModel.swift
@@ -0,0 +1,39 @@
+//
+//  ProductCellModel.swift
+//  Picnic
+//
+//  Created by dmytro.andreikiv@philips.com on 13/11/2017.
+//  Copyright © 2017 Dmytro Andreikiv. All rights reserved.
+//
+
+import Foundation
+
+struct ProductCellModel {
+	
+	private let product: Product
+	private let numberFormatter: NumberFormatter
+	
+	var name: String {
+		return product.name
+	}
+	
+	var price: String? {
+		let productPrice = Double(product.price) / 100.0
+		return numberFormatter.string(from: NSNumber(floatLiteral: productPrice))
+	}
+	
+	init(product: Product) {
+		self.product = product
+		numberFormatter = NumberFormatter()
+		numberFormatter.currencySymbol = "€"
+		numberFormatter.minimumIntegerDigits = 1
+		numberFormatter.minimumFractionDigits = 2
+		numberFormatter.maximumFractionDigits = 2
+		numberFormatter.numberStyle = .currency
+	}
+	
+	static func model(for product: Product) -> ProductCellModel {
+		return ProductCellModel(product: product)
+	}
+}
+
