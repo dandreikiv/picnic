31c0df69847df142904bc0af52c2283b905fcf1f
Dmytro Andreikiv <dmytro.andreikiv@philips.com>
2017-11-13 23:34:50 +0100

    Modified cell to configure itself with model

--- Picnic/Views/ProductCell.swift
+++ Picnic/Views/ProductCell.swift
@@ -10,12 +10,22 @@ import UIKit
 
 class ProductCell: UICollectionViewCell {
 	
+	static let identifier = String(describing: ProductCell.self)
+	
 	@IBOutlet var productImage: UIImageView!
 	@IBOutlet var productName: UILabel!
 	@IBOutlet var productPrice: UILabel!
 	
 	override func awakeFromNib() {
         super.awakeFromNib()
+		layer.borderWidth = 1
+		productImage.layer.borderWidth = 2
+		productImage.layer.borderColor = UIColor.green.cgColor
         // Initialization code
     }
+	
+	func configure(with model: ProductCellModel) {
+		productName.text = model.name
+		productPrice.text = model.price
+	}
 }
