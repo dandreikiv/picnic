5e69fec52105c53eb3f9b8bcab3fd032b2e416f4
Dmytro Andreikiv <dmytro.andreikiv@philips.com>
2017-11-13 20:20:46 +0100

    Added Product & ProductDetails models.

---
+++ Picnic/Models/Product.swift
@@ -0,0 +1,16 @@
+//
+//  Product.swift
+//  Picnic
+//
+//  Created by dmytro.andreikiv@philips.com on 13/11/2017.
+//  Copyright © 2017 Dmytro Andreikiv. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+	var productId: String
+	var name: String
+	var price: Int
+	var image: String
+}
---
+++ Picnic/Models/ProductDetails.swift
@@ -0,0 +1,17 @@
+//
+//  ProductDetails.swift
+//  Picnic
+//
+//  Created by dmytro.andreikiv@philips.com on 13/11/2017.
+//  Copyright © 2017 Dmytro Andreikiv. All rights reserved.
+//
+
+import Foundation
+
+struct ProductDetails {
+	var productId: String
+	var name: String
+	var price: Int
+	var image: String
+	var description: String
+}
